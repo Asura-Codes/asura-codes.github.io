a44db8a173bdd3606788823657f3c606fe5a1879
Patryk Tomaszewski <tomaszewski-patryk@o2.pl>
2025-06-27 15:00:56 +0200

    Updates

---
+++ assets/i18n-K7Jdy_c6.js
@@ -0,0 +1,1 @@
+import{d as s}from"./index-CjqstX3K.js";import{m as o}from"./index-CNlnWkbU.js";import{c as l}from"./vue-i18n-DNufKce7.js";const r=s(({app:e})=>{const a=l({locale:"en-US",fallbackLocale:"en-US",legacy:!1,messages:o,warnHtmlMessage:!1});e.use(a)});export{r as default};
